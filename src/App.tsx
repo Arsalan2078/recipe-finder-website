be4d0f9e5e215d12935080e423dbba101d072015
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-12-03 19:10:14 -0500

    starting styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
 export default function App() {
-    return <div>App</div>;
+    const sentence = "Beyond the lake lies a deeper, darker ocean green.";
+
+    return (
+        <div>
+            <div data-textPreset="1">{sentence}</div>
+            <div data-textPreset="2">{sentence}</div>
+            <div data-textPreset="3">{sentence}</div>
+            <div data-textPreset="4">{sentence}</div>
+            <div data-textPreset="5">{sentence}</div>
+            <div data-textPreset="6">{sentence}</div>
+            <div data-textPreset="7">{sentence}</div>
+            <div data-textPreset="8">{sentence}</div>
+            <div data-textPreset="9">{sentence}</div>
+            <div data-textPreset="10">{sentence}</div>
+        </div>
+    );
 }
